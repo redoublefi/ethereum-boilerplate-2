ae8f0b58a2f1c538166b496a6092c032f607ec92
Shalini Krishna <skrishnam108@gmail.com>
2022-03-31 18:37:25 +0800

    Added Stats card template on wallet page - can be used elsewhere later.

--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -2,7 +2,7 @@
 import NativeBalance from "../NativeBalance";
 import Address from "../Address/Address";
 import Blockie from "../Blockie";
-import { Card } from "antd";
+import { Card, Statistic, Row, Col, Button } from "antd";
 
 const styles = {
   title: {
@@ -27,17 +27,44 @@ const styles = {
 
 export default function Wallet() {
   return (
-    <Card
+    <div style={{ display: "flex", gap: "10px" }}>
+      <Card
       style={styles.card}
       title={
         <div style={styles.header}>
           <Blockie scale={5} avatar currentWallet style />
           <Address size="6" copyable />
           <NativeBalance />
+          
         </div>
+          
       }
     >
       {/* <Transfer /> */}
     </Card>
+    <Card
+      style={styles.card}
+      title={
+        <div style={styles.header}>
+          <Row gutter={16}>
+    <Col span={12}>
+      <Statistic title="Active Users" value={112893} />
+    </Col>
+    <Col span={12}>
+      <Statistic title="Account Balance (CNY)" value={112893} precision={2} />
+      <Button style={{ marginTop: 16 }} type="primary">
+        Recharge
+      </Button>
+    </Col>
+  </Row>
+          
+        </div>
+          
+      }
+    >
+      {/* <Transfer /> */}
+    </Card>  
+    </div>
+    
   );
 }
